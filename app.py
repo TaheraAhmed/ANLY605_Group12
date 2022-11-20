8e54f69820fed9617ed56c1280878dddfadaf311
Tahera Ahmed <ahmadtahera.ta@gmail.com>
2022-11-20 17:50:48 -0500

    app.py updates

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.header("InstaCart Reordering Prediction App")
 df = pd.read_csv('train_dataset.csv')
 
 fig = alt.Chart(df).mark_boxplot().encode(
-    x='reordered', y='uxp_reorder_ratio', size='c', color='c')
+    x='reordered:O', y='uxp_reorder_ratio:Q', size='c', color='c')
         
 st.altair_chart(fig, use_container_width=True)
 
